7d005dd1223b4b174f71160f3d7368e44774303d
roncsak <tom.roncsak@gmail.com>
2024-08-02 19:12:55 +0200

    add solution

--- test/dummy.test.ts
+++ test/dummy.test.ts
@@ -1,8 +1,9 @@
 import { createPinia, setActivePinia } from 'pinia';
-import { beforeEach, describe, expect, it } from 'vitest';
+import { beforeEach, describe, expect, it, vi } from 'vitest';
 import { createApp } from 'vue';
 import { MyPiniaPlugin } from '../src/store/plugins/myPiniaPlugin';
 import { useDummyStore } from '../src/store/dummy';
+import { MyFunctions } from '../src/plugins/functions';
 
 describe('Test Dummy Store', () => {
   const app = createApp({});
@@ -17,6 +18,10 @@ describe('Test Dummy Store', () => {
       it("should return 'This is a mock string'", () => {
       
         const store = useDummyStore();
+
+        // This line was provided by Vladimir (vova.dev) on Discord
+        // https://discord.com/channels/917386801235247114/1268956227770126356/1268961100112855143
+        MyFunctions.prototype.getMessage = vi.fn().mockReturnValue('This is a mock text.');
   
         expect(store.getMessage()).toBe('This is a mock text.');
       });
